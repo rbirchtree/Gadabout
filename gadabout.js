2b327e9d4768768cb36749db46af7b2fb2203c8f
Rob <rbirchtree@gmail.com>
2017-12-28 01:21:36 -0600

    revisions to css and js

--- gadabout.js
+++ gadabout.js
@@ -1,57 +1,99 @@
-
-$(function(){ 
-  let cityPositions;
-  navigator.geolocation.getCurrentPosition(function(position){
-   cityPositions = [position.coords.latitude, position.coords.longitude];
-   /*pass data into currentCityWeatherAPICall*/
-   /*https://maps.googleapis.com/maps/api/geocode/json?address=1309nightingaleaustintx&key=AIzaSyAXjEyA_kfZE3rPEHYM6B1j1yJTZwehan4*/
-   var currentCity;
-   var coords
-   currentCityWeatherAPICall(cityPositions);
+$(function(){
+	navigator.geolocation.getCurrentPosition(function(position) {
+  		var userGeoCodes = [position.coords.latitude, position.coords.longitude];
+  		GeoCodeToCityLookup(userGeoCodes);	
+	});
+	});
+	$("#eventFinder").submit(event => {
+		event.preventDefault();
+		userTravelCity = $("#city").val();
+		userInterest = $("#userInterest").val();
+		$("#results").removeClass("hidden");
+		$("#travelCityWeather").removeClass("hidden");
+		googleLookupCityForLatLon(userTravelCity,userInterest);
+		travelCityWeatherAPICall(userTravelCity)
+		$("#userInterest").val("");
+		$("#city").val("");
+		$("#results").empty();
+	});
    
-   
-  $("#eventFinder").submit(event => {
-    event.preventDefault();
-    var location = $("#city").val();' replace spaces with forms use a better name for location variable, test city and state combos'
-    var interests = $("#usersInterest").val();
-    $("#results").removeClass("hidden"); 'user results for a variable'
-    $("#travelCityWeather").removeClass("hidden");
-     $("container").html(`<ul class="hidden" id="results"></ul>`);
 
-    let cityForAPI = encodeURI(location);
-    googleLookupCityForLatLon(cityForAPI)
-    callMeetUPAPI(location,interests);
-	travelCityWeatherAPICall(cityForAPI);
-    
-    $("#usersInterest").val("");
-    $("#city").val("");
-    $("#results").empty();
-  }); 
+function GeoCodeToCityLookup(geocodes){
+	$.ajax({
+		method: 'GET',
+		url: `https://maps.googleapis.com/maps/api/geocode/json?latlng=${geocodes[0]},${geocodes[1]}&key=AIzaSyAXjEyA_kfZE3rPEHYM6B1j1yJTZwehan4`,
+		success: function(data){
+			/*return city geococe later tonight*/
+			var userCity = data.results[3].address_components["0"].long_name;
+			$("#currentLocationWeather").html(`<h2>${userCity}'s Weather</h2>`)
+			currentCityWeatherAPICall(geocodes);
+
+        }
 });
+}
 
-function callMeetUPAPI(city, interests){
-  
- /*run google function here and return coordinates*/
- googleLookupCityForLatLon(city);
-            /*let coords = latLon[city.toUpperCase()];*/
-            
+  function currentCityWeatherAPICall(geocodes){
+    $.ajax({
+      method: 'GET',
+      url: `https://api.openweathermap.org/data/2.5/weather?lat=${geocodes[0]}&lon=${geocodes[1]}&mode=json&units=imperial&appid=71dd692b6e018b9ef955bdbff87a0067`,
+      success: function(weather_data){
+        let weatherImage = weatherImages[weather_data.weather[0].description];
+        weatherImage === undefined ? weatherImage = "<img src='https://img.buzzfeed.com/buzzfeed-static/static/2015-04/22/12/enhanced/webdr12/enhanced-buzz-4479-1429721520-8.jpg'/>" : weatherImage;
+        $("#currentLocationWeather").append(weatherImage);    
+      }
+    });
+  } 
+
+function googleLookupCityForLatLon(travelCity,userInterest){
+   		$.ajax({
+   			method: 'GET',
+   			url: `https://maps.googleapis.com/maps/api/geocode/json?address=${travelCity}&key=AIzaSyAXjEyA_kfZE3rPEHYM6B1j1yJTZwehan4`,
+   			success: function (mapData){
+				 let travelCityGeoCodes = [mapData.results["0"].geometry.location.lat, mapData.results["0"].geometry.location.lng];
+				 /*return travel city name*/
+				 userTravelCity = mapData.results["0"].address_components["0"].long_name				
+				 callMeetUPAPI(travelCityGeoCodes, userInterest);
+
+   			},
+   			/*add error handaling here*/
+   			error: function(){
+   				$("#results").html("<p>Please enter a valid location.</p>");
+   			}
+   		});
+   }
+
+function travelCityWeatherAPICall(city){
+    $.ajax({
+      method: 'GET',
+      url: `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(city)}&mode=json&units=imperial&appid=71dd692b6e018b9ef955bdbff87a0067`,
+      success: function(weather_data){
+        let image = weatherImages[weather_data.weather[0].description];
+        
+        image === undefined ? image = "<img src='https://img.buzzfeed.com/buzzfeed-static/static/2015-04/22/12/enhanced/webdr12/enhanced-buzz-4479-1429721520-8.jpg'/>" : image;
+        $("#travelCityWeather").html(`${image}`);
+        $('#travelCityWeather').find('img').before(`<h2>${weather_data.name}'s Weather</h2>`);
+
+      }
+    });
+  }
+
+
+function callMeetUPAPI(geocodes, interests){
   $.ajax({
     method:'GET',
     crossDomain: true,
     dataType: 'jsonp',
-    url: `https://api.meetup.com/2/open_events?&sign=true&photo-host=public&lat=${coords[0]}&topic=${encodeURI(interests)}&lon=${coords[1]}&radius=40&page=3&key=434519614563187d65466f42b4e6b74`,
+    url: `https://api.meetup.com/2/open_events?&sign=true&photo-host=public&lat=${geocodes[0]}&topic=${encodeURI(interests)}&lon=${geocodes[1]}&radius=50&page=3&key=434519614563187d65466f42b4e6b74`,
     success: function(meetupData){
-      if(meetupData.meta.count===0) {
+    	
+      if(meetupData.meta.count === 0) {
+      	console.log(meetupData)
         $("#results").html("<h2>There are no results for that in this town.</h2>");
       } else {
-      $('#results').html(`<h2>Events in ${city.toUpperCase()}</h2>`);
+      $('#results').html(`<h2>Events in ${userTravelCity}</h2>`);
+
       jQuery.each( meetupData.results, function( i ) {
-        $('#results').append(
-          `<li><a href=" ${meetupData.results[i].event_url}"> ${meetupData.results[i].name}</a> ${moment(meetupData.results[i].time).add(1,'hour').local().format('MMM DD hh:mm a')}</li>`);
-        /*https://stackoverflow.com/questions/26392280/using-momentjs-to-convert-date-to-epoch-then-back-to-date*/
-        /*${moment.unix(meetupData.results[i].time).format('hh:mm a')*/
-        /*AIzaSyAXjEyA_kfZE3rPEHYM6B1j1yJTZwehan4 google api key*/
-        /*https://developers.google.com/maps/documentation/geocoding/start*/
+        $('#results').append(`<li><a href=" ${meetupData.results[i].event_url}"> ${meetupData.results[i].name}</a> ${moment(meetupData.results[i].time).add(1,'hour').local().format('MMM DD hh:mm a')}</li>`);
           return (i < 6);
           }
         );
@@ -63,47 +105,6 @@ function callMeetUPAPI(city, interests){
   });
 }  
 
-function travelCityWeatherAPICall(city){
-    $.ajax({
-      method: 'GET',
-      url: `https://api.openweathermap.org/data/2.5/weather?q=${city}&mode=json&units=imperial&appid=71dd692b6e018b9ef955bdbff87a0067`,
-      success: function(weather_data){
-        let image = weatherImages[weather_data.weather[0].description];
-        
-        image === undefined ? image = "<img src='https://img.buzzfeed.com/buzzfeed-static/static/2015-04/22/12/enhanced/webdr12/enhanced-buzz-4479-1429721520-8.jpg'/>" : image;
-        $("#travelCityWeather").html(`${image}`);
-        $('#travelCityWeather').find('img').before(`<p>${jsUcfirst(decodeURI(city))}'s Weather</p>`);
-      }
-    });
-  }
-  
-  function currentCityWeatherAPICall(positions){
-    $.ajax({
-      method: 'GET',
-      url: `https://api.openweathermap.org/data/2.5/weather?lat=${positions[0]}&lon=${positions[1]}&mode=json&units=imperial&appid=71dd692b6e018b9ef955bdbff87a0067`,
-      success: function(weather_data){
-        let image = weatherImages[weather_data.weather[0].description];
-        image === undefined ? image = "<img src='https://img.buzzfeed.com/buzzfeed-static/static/2015-04/22/12/enhanced/webdr12/enhanced-buzz-4479-1429721520-8.jpg'/>" : image;
-        $("#currentLocationWeather").html(`<p> Current Location's Weather </p>`+image);    
-
-      }
-    });
-  } 
-   	function googleLookupCityForLatLon(city){
-   		$.ajax({
-   			method: 'GET',
-   			url: `https://maps.googleapis.com/maps/api/geocode/json?address=${city}&key=AIzaSyAXjEyA_kfZE3rPEHYM6B1j1yJTZwehan4`,
-   			success: function (mapData){
-				 coords = [mapData.results["0"].geometry.location.lat, mapData.results["0"].geometry.location.lng];
-				return coords;
-				/*append to reuslts*/
-				'have coords and user interest be passed into google look up for the city in meet-up api, turn async back to true'
-   			},
-   			async: false
-   		});
-   }
-
-});
 
 const weatherImages = {
       "clear sky": "<img src='cloudsblue.jpg'/>",
@@ -112,12 +113,6 @@ const weatherImages = {
       "snow":"<img src='http://png.clipart.me/graphics/thumbs/160/snowflakes-winter-frosty-snow-background_160742723.jpg'/>",
       "overcast clouds":"<img src='https://img.buzzfeed.com/buzzfeed-static/static/2015-04/22/12/enhanced/webdr12/enhanced-buzz-4479-1429721520-8.jpg'/>"
     };
-
-
-
-function jsUcfirst(string){
-	return string.charAt(0).toUpperCase()+string.slice(1);
-}
 
 
 function meetupTime(dataTime){
