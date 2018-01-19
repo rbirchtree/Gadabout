274b2950abdc17d03770b021ee4a8889fd95d873
Rob <rbirchtree@gmail.com>
2018-01-18 21:23:57 -0600

    minor revisions

--- gadabout.js
+++ gadabout.js
@@ -49,20 +49,21 @@ function googleLookupCityForLatLon(travelCity,userInterest){
    			method: 'GET',
    			url: `https://maps.googleapis.com/maps/api/geocode/json?address=${travelCity}&key=AIzaSyAXjEyA_kfZE3rPEHYM6B1j1yJTZwehan4`,
    			success: function (mapData){
+          if (mapData.status!="ZERO_RESULTS"){
 				 let travelCityGeoCodes = [mapData.results["0"].geometry.location.lat, mapData.results["0"].geometry.location.lng];
 				 /*return travel city name map data zero results error handaling code from here when using xkcd city*/
 				 
 				 userTravelCity = mapData.results["0"].address_components["0"].long_name				
 				 callMeetUPAPI(travelCityGeoCodes, userInterest);
-
+          } else{
+            $("#results").html("<p>Please enter a valid location or interest.</p>")
+          }
    			},
    			/*add error handaling here*/
    			error: function(){
-   				$("#results").html("<p>Please enter a valid location.</p>");
+   				$("#results").html("<p>PThere has been an error.</p>");
    			},
-        complete: function(){
-          $("#results").html("<p>Please enter a valid location or interest.</p>")
-        },
+
         statusCode: {
             404: function() {
       $( "#results" ).html("<p>Please enter a valid location.");
@@ -94,13 +95,12 @@ function callMeetUPAPI(geocodes, interests){
     dataType: 'jsonp',
     url: `https://api.meetup.com/2/open_events?&sign=true&photo-host=public&lat=${geocodes[0]}&topic=${encodeURI(interests)}&lon=${geocodes[1]}&radius=70&page=3&key=434519614563187d65466f42b4e6b74`,
     success: function(meetupData){
-    	
-      if(meetupData.meta.count === 0) {
-      	console.log(meetupData)
+      if(meetupData.meta.count == 0) {
         $("#results").html("<h2>There are no results for that in this town.</h2>");
+      } else if (meetupData.meta.count === undefined){
+        $("#results").html("<h2>There are no results for that.</h2>");
       } else {
       $('#results').html(`<h2>Events in ${userTravelCity}</h2>`);
-
       jQuery.each( meetupData.results, function( i ) {
         $('#results').append(`<li><a href=" ${meetupData.results[i].event_url}"> ${meetupData.results[i].name}</a> ${moment(meetupData.results[i].time).add(1,'hour').local().format('MMM DD hh:mm a')}</li>`);
           return (i < 6);
